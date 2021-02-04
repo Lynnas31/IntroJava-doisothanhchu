62d36035bf3a80e0d7fa3ec5ad912c315daa658f
Hien_Nguyen <h.phuong211296@gmail.com>
2021-02-04 16:28:14 +0700

    2

--- src/convert.java
+++ src/convert.java
@@ -29,6 +29,25 @@ public class convert {
                         System.out.println(soThanhChu[hangChuc] + " mươi " + soThanhChu[hangDonVi]);
                     }
                 }
+            } else if (so <1000){
+                int hangDonvi = so % 10;
+                int hangChuc = (so /10) %10;
+                int hangTram = so / 100;
+                if (hangChuc == 1){
+                    if (hangDonvi == 0){
+                        System.out.println(soThanhChu[hangTram]+"trăm" + "mười");
+                    } else {
+                        System.out.println(soThanhChu[hangTram]+"trăm"+ "mười" +soThanhChu[hangDonvi]);
+                    }
+                } else {
+                    if (hangDonvi==0) {
+                        System.out.println(soThanhChu[hangTram]+"trăm"+soThanhChu[hangDonvi]+"mươi");
+                    } else {
+                        System.out.println(soThanhChu[hangTram] + " trăm " + soThanhChu[hangChuc] + " mươi " + soThanhChu[hangDonvi]);
+
+                    }
+                }
+
             }
         }
     }
